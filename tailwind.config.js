2846ec7f658d67f796a968abfa6dab8bf9870087
Melody Chukwuma <Melodyoluchi848@gmail.com>
2025-11-15 09:15:41 +0100

    Feat: add,try burnt orange colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,25 @@
-// tailwind.config.js
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'burnt-orange': {
+          50: '#fef8f4',
+          100: '#fde8d8',
+          200: '#fbd0b0',
+          300: '#f8af7d',
+          400: '#f4834a',
+          500: '#f16a22',
+          600: '#e24f17',
+          700: '#bd3c15',
+          800: '#973119',
+          900: '#7a2c18',
+        }
+      }
+    },
   },
   plugins: [],
 }
